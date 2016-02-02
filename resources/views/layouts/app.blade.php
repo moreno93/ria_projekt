e17bc196edb8333f436146e5565c8dd9d1fb765d
elicul <elicul@riteh.hr>
2016-02-02 20:17:22 +0100

    Ispravljen css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,12 +13,13 @@
 
     <!-- Styles -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
-    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="css/londinium-theme.css" rel="stylesheet" type="text/css">
-    <link href="css/styles.css" rel="stylesheet" type="text/css">
-    <link href="css/icons.css" rel="stylesheet" type="text/css">
+    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    <link href="{{{ asset('css/bootstrap.min.css') }}}" rel="stylesheet"> 
+    <link href="{{{ asset('css/londinium-theme.css') }}}" rel="stylesheet"> 
+    <link href="{{{ asset('css/styles.css') }}}" rel="stylesheet"> 
+    <link href="{{{ asset('css/icons.css') }}}" rel="stylesheet"> 
+
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&amp;subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
 
     
@@ -83,37 +84,39 @@
     <!-- JavaScripts -->
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
+    
+    <script src="{{ asset('js/plugins/charts/sparkline.min.js') }}"></script>
 
-    <script type="text/javascript" src="js/plugins/charts/sparkline.min.js"></script>
+    <script src="{{ asset('js/plugins/forms/uniform.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/forms/select2.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/forms/inputmask.js') }}"></script>
+    <script src="{{ asset('js/plugins/forms/autosize.js') }}"></script>
+    <script src="{{ asset('js/plugins/forms/inputlimit.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/forms/listbox.js') }}"></script>
+    <script src="{{ asset('js/plugins/forms/multiselect.js') }}"></script>
+    <script src="{{ asset('js/plugins/forms/validate.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/forms/tags.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/forms/switch.min.js') }}"></script>
 
-    <script type="text/javascript" src="js/plugins/forms/uniform.min.js"></script>
-    <script type="text/javascript" src="js/plugins/forms/select2.min.js"></script>
-    <script type="text/javascript" src="js/plugins/forms/inputmask.js"></script>
-    <script type="text/javascript" src="js/plugins/forms/autosize.js"></script>
-    <script type="text/javascript" src="js/plugins/forms/inputlimit.min.js"></script>
-    <script type="text/javascript" src="js/plugins/forms/listbox.js"></script>
-    <script type="text/javascript" src="js/plugins/forms/multiselect.js"></script>
-    <script type="text/javascript" src="js/plugins/forms/validate.min.js"></script>
-    <script type="text/javascript" src="js/plugins/forms/tags.min.js"></script>
-    <script type="text/javascript" src="js/plugins/forms/switch.min.js"></script>
 
-    <script type="text/javascript" src="js/plugins/forms/uploader/plupload.full.min.js"></script>
-    <script type="text/javascript" src="js/plugins/forms/uploader/plupload.queue.min.js"></script>
+    <script src="{{ asset('js/plugins/forms/uploader/plupload.full.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/forms/uploader/plupload.queue.min.js') }}"></script>
 
-    <script type="text/javascript" src="js/plugins/forms/wysihtml5/wysihtml5.min.js"></script>
-    <script type="text/javascript" src="js/plugins/forms/wysihtml5/toolbar.js"></script>
+    <script src="{{ asset('js/plugins/forms/wysihtml5/wysihtml5.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/forms/wysihtml5/toolbar.js') }}"></script>
 
-    <script type="text/javascript" src="js/plugins/interface/daterangepicker.js"></script>
-    <script type="text/javascript" src="js/plugins/interface/fancybox.min.js"></script>
-    <script type="text/javascript" src="js/plugins/interface/moment.js"></script>
-    <script type="text/javascript" src="js/plugins/interface/jgrowl.min.js"></script>
-    <script type="text/javascript" src="js/plugins/interface/datatables.min.js"></script>
-    <script type="text/javascript" src="js/plugins/interface/colorpicker.js"></script>
-    <script type="text/javascript" src="js/plugins/interface/fullcalendar.min.js"></script>
-    <script type="text/javascript" src="js/plugins/interface/timepicker.min.js"></script>
+    <script src="{{ asset('js/plugins/interface/daterangepicker.js') }}"></script>
+    <script src="{{ asset('js/plugins/interface/fancybox.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/interface/moment.js') }}"></script>
+    <script src="{{ asset('js/plugins/interface/jgrowl.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/interface/datatables.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/interface/colorpicker.js') }}"></script>
+    <script src="{{ asset('js/plugins/interface/fullcalendar.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/interface/timepicker.min.js') }}"></script>
 
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="js/application.js"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/application.js') }}"></script>
+
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
