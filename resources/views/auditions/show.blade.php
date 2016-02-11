1774ee4e086a47098a2f9cc129022f6426f748e3
arescec <arescec@yandex.com>
2016-02-11 19:43:39 +0100

    Stranica audicije uredjena

--- resources/views/auditions/show.blade.php
+++ resources/views/auditions/show.blade.php
@@ -1,36 +1,31 @@
 @extends('layouts.app')
 @section('content')
 
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-
 <div class="container">
+
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-
+                <br>
                 <div class="jumbotron">
-                  <h1>{{ $audition->audition_name }}</h1>
-                  <p>...</p>
-                  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
-                </div>
-
-
-                <!-- <div class="panel-heading">Dashboard</div>
-                    <div class="panel-body">
-                    Stranica audicije sa id-em {{ $audition->id }} i imenom {{ $audition->audition_name }}
-                        <br/>
-                        {{ $audition->description }}
-                            <div class="form-group">
-                                @if(Auth::user()->agency()->first())
+                    <h1><strong>{{ $audition->audition_name }}</strong></h1>
+                    <p>
+                        <span>Budget: <i>{{ $audition->budget }}</i></span>
+                        <br>
+                        <span>Location: <i>{{ $audition->city }},{{ $audition->country }}</i></span>
+                        <br>
+                        <span>Started: <i>{{ date('F d, Y', strtotime($audition->created_at)) }}</i></span>
+                    </p>
+                    <p>{{ $audition->description }}</p>
+                    <p>           
+                        @if(Auth::user()->agency()->first())
                                     @if(!Auth::user()->agency->auditions->contains($audition))
                                         @if(!$audition->users->contains(Auth::user())) 
                                             <div class="col-md-6 col-md-offset-4">
                                                 <a href ="/auditions/{{ $audition->id }}/apply">
-                                                <button type="button" class="btn btn-primary">
-                                                <i class="fa fa-btn">Apply To Audition</i>
-                                                </button>
+                                                    <button type="button" class="btn btn-primary">
+                                                        <i class="fa fa-btn">Apply To Audition</i>
+                                                    </button>
                                                 </a>
-                                                </button>
                                             </div>
                                         @else
                                             <div class="col-md-6 col-md-offset-4">
@@ -39,62 +34,98 @@
                                         @endif
                                     @endif
                                 @endif
+                    </p>
 
-                                @if(!Auth::user()->agency()->first())
-                                    @if(!$audition->users->contains(Auth::user()))
-                                        <div class="col-md-6 col-md-offset-4">
-                                            <a href ="/auditions/{{ $audition->id }}/apply">
+                        <div class="row">
+                            <div class="col-md-12">
+                            @if(Auth::user()->agency()->first())
+                                @if(Auth::user()->agency()->first()->id == $audition->agency_id)
+                                    
+                                    <div class="inline">
+                                        <a href ="/auditions/{{ $audition->id }}/edit">
                                             <button type="button" class="btn btn-primary">
-                                            <i class="fa fa-btn">Apply To Audition</i>
+                                                <i class="fa fa-btn">Edit</i>
                                             </button>
-                                            </a>
+                                        </a>
+                                    </div>
+
+                                    <div class="inline">
+                                        <a href ="/auditions/{{ $audition->id }}/users">
+                                            <button type="button" class="btn btn-primary">
+                                                <i class="fa fa-btn">Show Applied Users</i>
                                             </button>
-                                        </div>
-                                    @else
-                                        <div class="col-md-6 col-md-offset-4">
-                                            <h3>You are applied to this audition</h3>
-                                        </div>
-                                    @endif
+                                        </a>
+                                    </div>
+
+                                    <div class="inline">
+                                        <form class="delete" action="/auditions/{{ $audition->id }}" method="POST">
+                                            <input type="hidden" name="_method" value="DELETE">
+                                            <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                                            <button type="submit" class="btn btn-danger">
+                                                Delete
+                                            </button>
+                                        </form>
+                                    </div> 
+
                                 @endif
-
-                                @if(Auth::user()->agency()->first())
-                                    @if(Auth::user()->agency()->first()->id == $audition->agency_id)
-                                        
-                                        <div class="col-md-6 col-md-offset-4">
-                                            <a href ="/auditions/{{ $audition->id }}/edit">
-                                            <button type="button" class="btn btn-primary">
-                                            <i class="fa fa-btn">Edit</i>
-                                            </button>
-                                            </a>
-                                            </button>
-                                        </div>
-
-                                        <div class="col-md-6 col-md-offset-4">
-                                            <form class="delete" action="/auditions/{{ $audition->id }}" method="POST">
-                                                <input type="hidden" name="_method" value="DELETE">
-                                                <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-                                                <button type="submit" class="btn btn-danger">
-                                                    Delete
-                                                </button>
-                                            </form>
-                                        </div>
-                                        <div class="col-md-6 col-md-offset-4">
-                                            <a href ="/auditions/{{ $audition->id }}/users">
-                                            <button type="button" class="btn btn-primary">
-                                            <i class="fa fa-btn">Show Applied Users</i>
-                                            </button>
-                                            </a>
-                                            </button>
-                                        </div>
-                                    @endif
-                                @endif
+                            @endif
                             </div>
-                    </div>
-                </div> -->
-            </div>
+                        </div>
+                </div>
+
+                <div class="col-md-10 col-md-offset-1">
+                  <h2>Available roles</h2>         
+                  <table class="table table-hover table-responsive">
+                    <thead>
+                      <tr>
+                        <th>Role</th>
+                        <th>Positions</th>
+                        <th>Salary</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Director</td>
+                        <td>{{ $audition->num_directors }}</td>
+                        <td>{{ $audition->pay_directors }}</td>
+                      </tr>
+                      <tr>
+                        <td>Producer</td>
+                        <td>{{ $audition->num_producers }}</td>
+                        <td>{{ $audition->pay_producers }}</td>
+                      </tr>
+                      <tr>
+                        <td>Cameraman</td>
+                        <td>{{ $audition->num_cameraman }}</td>
+                        <td>{{ $audition->pay_cameraman }}</td>
+                      </tr>
+                      <tr>
+                        <td>Film Editor</td>
+                        <td>{{ $audition->num_film_editors }}</td>
+                        <td>{{ $audition->pay_film_editors }}</td>
+                      </tr>
+                      <tr>
+                        <td>Sound Designer</td>
+                        <td>{{ $audition->num_sound_designers }}</td>
+                        <td>{{ $audition->pay_sound_designers }}</td>
+                      </tr>
+                      <tr>
+                        <td>Actor</td>
+                        <td>{{ $audition->num_actors }}</td>
+                        <td>{{ $audition->pay_actors }}</td>
+                      </tr>
+                      <tr>
+                        <td>Extra</td>
+                        <td>{{ $audition->num_extras }}</td>
+                        <td>{{ $audition->pay_extras }}</td>
+                      </tr>                                                                                        
+                    </tbody>
+                  </table>
+                </div>
         </div>
     </div>
 </div>
+<br>
 
 <script>
     $(".delete").submit(function(){
