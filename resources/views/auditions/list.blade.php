04df89122e6a9cd628381596470008c6dd044a73
arescec <arescec@yandex.com>
2016-02-12 21:21:37 +0100

    Dodan list all za audicije

--- resources/views/auditions/list.blade.php
+++ resources/views/auditions/list.blade.php
@@ -1,18 +1,25 @@
 @extends('layouts.app')
 @section('content')
 	@foreach ($auditions as $audition)
+	<div class="col-md-12">
 		<article>
 			<h2>
 				<a href="{{ url('/auditions', $audition->id) }}">{{ $audition->audition_name }}</a>
 			</h2>
+			<p>{{ date('F d, Y', strtotime($audition->created_at)) }} - {{ $audition->city }}, {{ $audition->country }}</p>
+			<p>Budget: {{ $audition->budget }}</p>
+			<p><big>{{ $audition->description }}</big></p>
 		</article>
+		<hr>
+	</div>
 	@endforeach
 	<div class="form-group">
 		<a href="{{ Request::header("referer") }}">
 			<div class="col-md-6 col-md-offset-4">
 				<button type="submit" class="btn btn-danger">
-					<i class="fa fa-btn fa-plus"></i>Back
+					Back
 				</button>
+				<br>
 			</div>
 		</a>
 	</div>
